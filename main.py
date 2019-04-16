c44303a8a4cc446603006d3da674f3ed408e7123
sargamtiwari <43930217+sargamtiwari@users.noreply.github.com>
2019-04-16 16:11:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hey its Python Flask application created by sargam tiwari!'
+ import pandas as pd
+   import matplotlib.pyplot as plt
+   dfinterndata=pd.read_csv('data for Add in\interns.csv',sep=',', parse_dates=['YEAR'],index_col='YEAR')
+   print(dfinterndata)
+   plt.plot(dfinterndata)
+   train =dfinterndata[:int(0.8*(len(dfinterndata)))]
+   valid = dfinterndata[int(0.8*(len(dfinterndata))):]
+   from statsmodels.tsa.vector_ar.var_model import VAR
+   model = VAR(endog=train)
+   model_fit = model.fit()
+   prediction = model_fit.forecast(model_fit.y, steps=2)
+   #print(prediction)
+   cols=dfinterndata.columns
+   #print(cols)
+   interns = pd.DataFrame(index=range(0,len(prediction)),columns=[cols])
+   for j in range(0,10):
+    for i in range(0, len(prediction)):
+       interns.iloc[i][j] =round( prediction[i][j])
+  
+   df_list = interns.values.tolist()
+   JSONP_data = jsonify(df_list)
+   return JSONP_data
 
 if __name__ == '__main__':
   app.run()
